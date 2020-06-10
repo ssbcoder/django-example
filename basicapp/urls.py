d8a39fbcdd96a10466df6bf424e884d1c609df51
steffysaju <sajusteffy@gmail.com>
2020-06-10 10:49:46 +0530

    basic

---
+++ basicapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from basicapp import views
+
+
+app_name='basicapp'
+urlpatterns = [
+
+    path('register/',views.register,name='register'),
+    path('login/',views.user_login,name="user_login"),
+
+
+]
